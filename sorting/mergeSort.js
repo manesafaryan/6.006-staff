2a18b5916b48b9085870a817f33952dba251cca0
MANE SAFARYAN <manesafarian@gmail.com>
2021-12-12 21:54:06 +0400

    adding merge sort algo

---
+++ sorting/mergeSort.js
@@ -0,0 +1,52 @@
+function merge(arr, l, m, r) {
+    const l1 = m - l + 1;
+    const l2 = r - m;
+    const L = new Array(l1);
+    const R = new Array(l2);
+    for (let i = 0; i < l1; i++) {
+      L[i] = arr[l + i];
+    }
+    for (let i = 0; i < l2; i++) {
+      R[i] = arr[m + 1 + i];
+    }
+    let i = 0;
+    let j = 0;
+    let k = l;
+    while (i < l1 && j < l2) {
+      if(L[i] < R[j]) {
+        arr[k] = L[i];
+        i++;
+      }
+      else {
+        arr[k] = R[j];
+        j++;
+      }
+      k++
+    }
+    while(i < l1) {
+      arr[k] = L[i]
+      i++;
+      k++
+    }
+    while(j < l2) {
+      arr[k] = R[j]
+      j++;
+      k++
+    }
+  }
+  
+  function mergeSort(arr, l, r) {
+    if (l >= r) return;
+    let m = parseInt((r + l) / 2);
+    mergeSort(arr, l, m );
+    mergeSort(arr, m + 1, r);
+    merge(arr, l, m, r);
+  }
+  
+  function printArray(A, size) {
+    for (var i = 0; i < size; i++) console.log(A[i] + " ");
+  }
+  var arr = [12, 11, 13, 5, 6, 7, 58, 2];
+  var arr_size = arr.length;
+  mergeSort(arr, 0, arr_size - 1);
+  printArray(arr, arr_size);
